d76213edd8af4502aca40f184e9d2b8841339bf9
Jais Andersen <jais.h.sorensen@gmail.com>
2025-02-28 10:58:59 +0100

    TCP server og client

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+
+import random
+print(random.randrange(1,200))
+
+vegetableslist = ["Cucumber", "Tomato", "Salad"]
+print(vegetableslist)
+print(len(vegetableslist))
+
+fruitlist = list(("apple", "banana", "cherry"))
+print(fruitlist)
+
+vegetablestuple = ("cucumber", "Tomato", "Salad", "Onion")
+print(vegetablestuple)
+print(len(vegetablestuple))
