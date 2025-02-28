69b5f47e72a481b902df89fccb9e49351f98979e
Jais Andersen <jais.h.sorensen@gmail.com>
2025-02-28 11:45:16 +0100

    server updated

--- TCPEchoServer.py
+++ TCPEchoServer.py
@@ -1,15 +1,21 @@
 from socket import *
 import threading
+
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print("Server is ready to recieve")
+
 def handleClient(connectionSocket, adress):
-    sentence = connectionSocket.recv(1024).decode()
-    print(sentence)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    try:
+        sentence = connectionSocket.recv(1024).decode()
+        print(sentence)
+        capitalizedSentence = sentence.upper()
+        connectionSocket.send(capitalizedSentence.encode())
+    except:
+        print("fejl")
+    connectionSocket.close()
 
 isRunning = True
 while isRunning:    
